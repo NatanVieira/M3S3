494846ff1ce8f8e028b8745baa1b830c11d88f08
Natanael Vieira <natanaelvieira130@gmail.com>
2022-09-15 09:18:19 -0300

    M3S3 - Exercicio 03"

---
+++ rh/Models/Funcionario.cs
@@ -0,0 +1,10 @@
+
+
+public class Funcionario {
+
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Senha { get; set; }
+    public EPermissoes Permissao { get; set; }
+    public decimal Salario { get; set; }
+}
