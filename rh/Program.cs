8f7826023af71c63978958c0694e2da1f061ecaa
Natanael Vieira <natanaelvieira130@gmail.com>
2022-09-15 12:18:12 -0300

    M3S3 - Exercicio 07"

--- rh/Program.cs
+++ rh/Program.cs
@@ -1,3 +1,8 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+var key = Encoding.ASCII.GetBytes(Settings.Secret);
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(x => {
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    // x.AddScheme = JwtBearerExtensions.AddJwtBearer(x => {
+    //    x.RequireHttpsMetadata = false;
+    //    x.SaveToken = true;
+    //    x.TokenValidationParameters = new TokenValidationParameters {
+    //         ValidateIssuerSigningKey = new SymmetricSecurityKey(key),
+    //         ValidateIssuer =false,
+    //         ValidateAudience = false
+    //    }; 
+    // });
+}).AddJwtBearer(x => {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters{
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+});
 
 var app = builder.Build();
 
@@ -21,5 +49,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
