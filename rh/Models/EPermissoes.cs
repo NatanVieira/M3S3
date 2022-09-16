f2032d3b3e433d01d9cb25cd552b919c2d861239
Natanael Vieira <natanaelvieira130@gmail.com>
2022-09-16 11:33:39 -0300

    M3S3 - Exercicio 11

--- rh/Models/EPermissoes.cs
+++ rh/Models/EPermissoes.cs
@@ -1,12 +1,19 @@
 
 
 using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
 
 namespace rh.Models {
     public enum EPermissoes {
 
+        [XmlEnumAttribute("F")]
+        [Display(Description = "Funcionario")]
         Funcionario,
+        [XmlEnumAttribute("G")]
+        [Display(Description = "Gerente")]
         Gerente,
+        [XmlEnumAttribute("A")]
+        [Display(Description = "Admnistrador")]
         Administrador
     }
 }
